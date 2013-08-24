da5c8fc909837c22f82685ab6590e9a81b02b967
noix <MiniFee@boucher_s-1.acad.gst.uqam.ca>
2013-08-24 03:28:50 -0400

    Stucture de base
    Header/Footer et pseuro index

--- wp-content/themes/couac/header.php
+++ wp-content/themes/couac/header.php
@@ -8,7 +8,9 @@
  * @subpackage Twenty_Thirteen
  * @since Twenty Thirteen 1.0
  */
-?><!DOCTYPE html>
+?>
+
+<!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7" <?php language_attributes(); ?>>
 <![endif]-->
@@ -24,6 +26,11 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/reset.css" type="text/css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/grid.css" type="text/css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/screen.css" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Noticia+Text:400,400italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:800,400' rel='stylesheet' type='text/css'>  <script type="text/javascript" charset="utf-8" src="respond.min.js"></script>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
@@ -31,21 +38,29 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="page" class="hfeed site">
-		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</a>
-
-			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php get_search_form(); ?>
-				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
-		</header><!-- #masthead -->
-
-		<div id="main" class="site-main">
+	<div class="container_5">  <!-- Header --> 
+		<div class="head">
+			<div class="grid_3 logo">
+			<a href="#"><img src="<?php bloginfo('template_url'); ?>/images/logo.jpg"></a>
+			</div>
+			<div class="grid_2 alpha omega">
+				<div class="recherche">
+	        <input type="text" value="Recherche" name="s" id="s" onfocus="this.value==this.defaultValue?this.value='':null"/>
+	        <!-- <input type="submit" id="searchsubmit" value=">" /> -->
+				</div>
+				<div class="clear"></div>
+				<div class="menu">
+					<ul>
+						<li><a href="#">Articles</a></li>
+						<li><a href="#">Brèves</a></li>
+						<li><a href="#">Contribuer</a></li>
+						<li><a href="#">À propos</a></li>
+						<li><a href="#">S'abonner</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="clear"></div>
+	
--- wp-content/themes/couac/index.php
+++ wp-content/themes/couac/index.php
@@ -16,23 +16,129 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-		<?php if ( have_posts() ) : ?>
+<div class="large nospace"> <!-- Plogues --> 
+	<div class="container_5">
+		<div class="grid_5 appel nospace">
+			<ul>
+				<li><a href="#"><img src="<?php bloginfo('template_url'); ?>/soiree.png" alt="Soiree" /></a></li>
+				<li><a href="#"><img src="<?php bloginfo('template_url'); ?>/soiree.png" alt="Soiree" /></a></li>
+				<li><a href="#"><img src="<?php bloginfo('template_url'); ?>/soiree.png" alt="Soiree" /></a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="clear"></div>
+</div> 
+<div class="clear"></div>
 
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
+<div class="container_5"> <!-- Article --> 
+	<article>
+		<header>
+			<div class="boite">
+				<div class="titre">
+					<h2>P-6 et profilage politique</h2>
+					<h1><a href="#">Pour que le pire ne soit pas avenir</a></h1>
+				</div>
+				<div class="info">
+					<p>Isabelle Baez<br />
+						<span>16 mai 2013</span></p>
+					</div>
+				</div>		
+			<div class="grid_4 prefix_1">
+				<img src="<?php bloginfo('template_url'); ?>/photo1.png" alt="Photo" />
+			</div>
+		</header>
+		<div class="grid_3 prefix_1 suffix_1 corps">
+			<p>J’aimerais qu’on revienne aujourd’hui sur le côté positif du printemps 2012 parce qu’à ce moment-là on ne se doutait pas la chance qu’on avait.</p>
+<p>Quelques dates, quelques bonheurs à partager. Le 13 avril, notre première manif de nuit. On inaugurait une tradition qu’on n’osait alors même pas espérer! Combien d’arrestations à cette première manif ? Une. Une seule, mes chères souris! Avec un peu de chance, c’était un infiltré, peut-être même Sylvain qui se faisait les dents. <span><a href="#">lire la suite</a></span></p>
+		</div>
+		<div class="clear"></div>
+	</article>
+</div> 
+<div class="clear"></div>
 
-			<?php twentythirteen_paging_nav(); ?>
+<div class="large breve">
+	<div class="container_5">
+		<div class="grid_1 titre">
+			<h1>Solidarité de première classe</h1>
+		</div>
+		<div class="grid_3 texte">
+			<p>En Alabama, plusieurs itinérants ont tenu à manifester leurs solidarité vis-à-vis les passagers du bateau de croisière Carnival Triumph, qui ont été obligés de vivre dans des conditions déplorables durant quelques jours.<br />
+				<span>MUSIRONIE</span></p>
+		</div>
+		<div class="grid_1 autre">
+			<a href="#">D'autres brèves »</a>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+<div class="clear"></div>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
+<div class="container_5"> <!-- Article --> 
+	<article>
+		<header>
+			<div class="boite">
+				<div class="titre">
+					<h2>P-6 et profilage politique</h2>
+					<h1><a href="#">Pour que le pire ne soit pas avenir</a></h1>
+				</div>
+				<div class="info">
+					<p>Isabelle Baez<br />
+						<span>16 mai 2013</span></p>
+					</div>
+				</div>		
+				<div class="grid_4 prefix_1">
+					<img src="<?php bloginfo('template_url'); ?>/photo1.png" alt="Photo" />
+				</div>
+		</header>
+		<div class="grid_3 prefix_1 suffix_1 corps">
+			<p>J’aimerais qu’on revienne aujourd’hui sur le côté positif du printemps 2012 parce qu’à ce moment-là on ne se doutait pas la chance qu’on avait.</p>
+<p>Quelques dates, quelques bonheurs à partager. Le 13 avril, notre première manif de nuit. On inaugurait une tradition qu’on n’osait alors même pas espérer! Combien d’arrestations à cette première manif ? Une. Une seule, mes chères souris! Avec un peu de chance, c’était un infiltré, peut-être même Sylvain qui se faisait les dents. <span><a href="#">lire la suite</a></span></p>
+		</div>
+		<div class="clear"></div>
+	</article>
+</div> 
+<div class="clear"></div>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+<div class="large breve">
+	<div class="container_5">
+		<div class="grid_1 titre">
+			<h1>Solidarité de première classe</h1>
+		</div>
+		<div class="grid_3 texte">
+			<p>En Alabama, plusieurs itinérants ont tenu à manifester leurs solidarité vis-à-vis les passagers du bateau de croisière Carnival Triumph, qui ont été obligés de vivre dans des conditions déplorables durant quelques jours.<br />
+				<span>MUSIRONIE</span></p>
+		</div>
+		<div class="grid_1 autre">
+			<a href="#">D'autres brèves »</a>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+<div class="clear"></div>	
 
-<?php get_sidebar(); ?>
+<div class="container_5"> <!-- Article --> 
+	<article class="nophoto">
+		<header>
+			<div class="boite">
+				<div class="titre">
+					<h2>P-6 et profilage politique</h2>
+					<h1><a href="#">Pour que le pire ne soit pas avenir</a></h1>
+				</div>
+				<div class="info">
+					<p>Isabelle Baez<br />
+						<span>16 mai 2013</span></p>
+				</div>
+			</div>				
+		</header>
+
+		<div class="grid_3 prefix_1 suffix_1 corps">
+			<p>J’aimerais qu’on revienne aujourd’hui sur le côté positif du printemps 2012 parce qu’à ce moment-là on ne se doutait pas la chance qu’on avait.</p>
+			<p>Quelques dates, quelques bonheurs à partager. Le 13 avril, notre première manif de nuit. On inaugurait une tradition qu’on n’osait alors même pas espérer! Combien d’arrestations à cette première manif ? Une. Une seule, mes chères souris! Avec un peu de chance, c’était un infiltré, peut-être même Sylvain qui se faisait les dents. <span><a href="#">lire la suite</a></span></p>
+		</div>
+		<div class="clear"></div>
+	</article>
+</div> 
+<div class="clear"></div>
+
+
 <?php get_footer(); ?>
